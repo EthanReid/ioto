0fef6e59970bb531fcfd24fa4271dee8fb1f0bfd
mahahahajan <Tiklum@PulkitApple.local>
2015-08-22 15:39:53 -0700

    Everything working, make a Backup... Dont be Sydney ;)

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -3,6 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+
+Parse.initialize("wpvbhNsxxZam6HYa63vmudxBgJrasHXLq7WTxkKH", "WhODpEkC35r18jewjzrpw22KJwxLZJxbGQQcyxST");
+
 angular.module('todo', ['ionic'])
 
 .factory('Projects', function() {
