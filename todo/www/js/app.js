2cbae3c72bf27fb036d87339ca63e9460c606b47
mahahahajan <Tiklum@PulkitApple.local>
2015-08-22 22:55:18 -0700

    Stashed my App.js changes

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -92,30 +92,51 @@ angular.module('ioto').controller('dashboardController', ['$scope',
 
 angular.module('ioto').controller('createCampaignController', ['$scope',
     function ($scope) {
-
-    }
-]);
-
-angular.module('ioto').controller('campaignController', ['$scope',
-    function ($scope) {
-			alert('Something happened');
 			Parse.$ = jQuery;
 
 			Parse.initialize("wpvbhNsxxZam6HYa63vmudxBgJrasHXLq7WTxkKH", "WhODpEkC35r18jewjzrpw22KJwxLZJxbGQQcyxST");
 			var ProjectInfo = Parse.Object.extend("ProjectPage");
 		  var project = new ProjectInfo();
 
+			//TODO: Set up form and do project.set("Stuff")
+
 			project.save(null, {
-		    success: function(project) {
-		      // Execute any logic that should take place after the object is saved.
-		      alert('New object created with objectId: ' + campainObject.id);
-		    },
-		    error: function(project, error) {
-		      // Execute any logic that should take place if the save fails.
-		      // error is a Parse.Error with an error code and message.
-		      alert('Failed to create new object, with error code: ' + error.message);
-		    }
-		  });
+				success: function(project) {
+					// Execute any logic that should take place after the object is saved.
+					console.log('New object created with objectId: ' + project.id);
+				},
+				error: function(project, error) {
+					// Execute any logic that should take place if the save fails.
+					// error is a Parse.Error with an error code and message.
+					console.log('Failed to create new object, with error code: ' + error.message);
+				}
+
+			});
+    }
+]);
+
+angular.module('ioto').controller('campaignController', ['$scope',
+    function ($scope) {
+			Parse.$ = jQuery;
+
+			Parse.initialize("wpvbhNsxxZam6HYa63vmudxBgJrasHXLq7WTxkKH", "WhODpEkC35r18jewjzrpw22KJwxLZJxbGQQcyxST");
+			var ProjectInfo = Parse.Object.extend("ProjectPage");
+		  var project = new ProjectInfo();
+			//alert('' +project);
+			var pTitle = project.get("Title");
+			var pDescription = project.get("Description");
+			var pTime = project.get("When");
+			var pPlace = project.get("Where");
+			var pHost = project.get("Contact");
+			var pMoney = project.get("MoneyNeeded");
+			var pTag = project.get("Tag");
+
+			var title = document.getElementById("Title");
+			var description = document.getElementById("description");
+			var time = document.getElementById("when");
+			var place = document.getElementById("where");
+			var host = document.getElementById("who");
+
     }
 ]);
 
