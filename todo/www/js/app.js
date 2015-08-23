ff9428e6ba1bdba1c342bf647bb9469501d9e09c
Mike Cao <mike@mikecao.net>
2015-08-22 21:05:51 -0700

    create view templates and code cleanup

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -1,120 +1,75 @@
-// Ionic Starter App
+angular.module('ioto', [
+	'ionic'
+]);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
+angular.module('ioto').factory('iotoFactory',
+	function () {
+		
+	}
+);
 
-angular.module('todo', ['ionic'])
+angular.module('owm').config(['$routeProvider', '$httpProvider',
+    function ($routeProvider, $httpProvider) {
+        $httpProvider.interceptors.push('httpInterceptorFactory');
 
-.factory('Projects', function() {
-  return {
-    all: function() {
-      var projectString = window.localStorage['projects'];
-      if(projectString) {
-        return angular.fromJson(projectString);
-      }
-      return [];
-    },
-    save: function(projects) {
-      window.localStorage['projects'] = angular.toJson(projects);
-    },
-    newProject: function(projectTitle) {
-      // Add a new project
-      return {
-        title: projectTitle,
-        tasks: []
-      };
-    },
-    getLastActiveIndex: function() {
-      return parseInt(window.localStorage['lastActiveProject']) || 0;
-    },
-    setLastActiveIndex: function(index) {
-      window.localStorage['lastActiveProject'] = index;
-    }
-  }
-})
+        $routeProvider
+        .when('/dashboard/:id', {
+            templateUrl: 'views/dashboard.html',
+            controller: 'dashboardController',
+            activetab: 'dashboard',
+            resolve: {
+                'user': [ '$route', 'userFactory',
+                function($route, userFactory) {
+                    return userFactory.getUser($route.current.params.id)
+                    .then(function(res){
+                        return res.data;
+                    })
+                    .catch(function(error){
+                        return null;
+                    });
+                }
+            ]}
+        })
+        .when('/dashboard/create-campaign', {
+            templateUrl: 'views/create-campaign.html',
+            controller: 'createCampaignController',
+            activetab: 'dashboard'
+        })
+        .when('/campaigns', {
+            templateUrl: 'views/campaigns.html',
+            controller: 'campaignsController',
+            activetab: 'home'
+        })
+        .when('/campaign/:id', {
+            templateUrl: 'views/create-campaign.html',
+            controller: 'createCampaignController',
+            activetab: 'dashboard',
+            resolve: {
+                'campaign': [ '$route', 'campaignFactory',
+                function($route, campaignFactory) {
+                    return campaignFactory.getCampaign($route.current.params.id)
+                    .then(function(res){
+                        return res.data;
+                    })
+                    .catch(function(error){
+                        return null;
+                    });
+                }
+            ]}
+        })
+        .when('/', {
+          templateUrl: 'views/home.html',
+          controller: 'homeController',
+          activetab: 'home'
+        })
+        .otherwise('/');
+	}
+]);
 
-.controller('TodoCtrl', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
-
-  // A utility function for creating a new project
-  // with the given projectTitle
-  var createProject = function(projectTitle) {
-    var newProject = Projects.newProject(projectTitle);
-    $scope.projects.push(newProject);
-    Projects.save($scope.projects);
-    $scope.selectProject(newProject, $scope.projects.length-1);
-  }
-
-
-  // Load or initialize projects
-  $scope.projects = Projects.all();
-
-  // Grab the last active, or the first project
-  $scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
-
-  // Called to create a new project
-  $scope.newProject = function() {
-    var projectTitle = prompt('Project name');
-    if(projectTitle) {
-      createProject(projectTitle);
-    }
-  };
-
-  // Called to select the given project
-  $scope.selectProject = function(project, index) {
-    $scope.activeProject = project;
-    Projects.setLastActiveIndex(index);
-    $ionicSideMenuDelegate.toggleLeft(false);
-  };
-
-  // Create our modal
-  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
-    $scope.taskModal = modal;
-  }, {
-    scope: $scope
-  });
-
-  $scope.createTask = function(task) {
-    if(!$scope.activeProject || !task) {
-      return;
-    }
-    $scope.activeProject.tasks.push({
-      title: task.title
-    });
-    $scope.taskModal.hide();
-
-    // Inefficient, but save all the projects
-    Projects.save($scope.projects);
-
-    task.title = "";
-  };
-
-  $scope.newTask = function() {
-    $scope.taskModal.show();
-  };
-
-  $scope.closeNewTask = function() {
-    $scope.taskModal.hide();
-  }
-
-  $scope.toggleProjects = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
+angular.module('ioto').controller('indexController', ['$scope', '$timeout', '$ionicModal', '$ionicSideMenuDelegate',
+	function ($scope, $timeout, $ionicModal, $ionicSideMenuDelegate) {
 
 
-  // Try to create the first project, make sure to defer
-  // this by using $timeout so everything is initialized
-  // properly
-  $timeout(function() {
-    if($scope.projects.length == 0) {
-      while(true) {
-        var projectTitle = prompt('Your first project title:');
-        if(projectTitle) {
-          createProject(projectTitle);
-          break;
-        }
-      }
-    }
-  });
 
-})
+	}
+]);
